f4d60615d052c33f96f34064119c0f3617121a8d
wangjunming <qjyn1314@163.com>
2020-10-28 15:11:47 +0800

    添加打印SQL语句配置

--- day-common/src/main/java/com/day/common/datasource/MybatisPlusConfig.java
+++ day-common/src/main/java/com/day/common/datasource/MybatisPlusConfig.java
@@ -1,8 +1,14 @@
 package com.day.common.datasource;
 
+import com.alibaba.druid.stat.DruidStatManagerFacade;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -17,6 +23,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan(basePackages = {
         "com.day.*.persistence.mapper",
 })
+@Slf4j
 public class MybatisPlusConfig {
 
+    @PostConstruct
+    public void dataSourcesToString(){
+        //参考与：https://github.com/alibaba/druid/tree/master/druid-spring-boot-starter
+        final List<Map<String, Object>> dataSourceStatDataList = DruidStatManagerFacade.getInstance().getDataSourceStatDataList();
+        log.info("数据源信息是：{}", dataSourceStatDataList);
+    }
 }
