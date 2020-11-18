d84b607136a53e973a7722bb885108745020e783
wangjunming <qjyn1314@163.com>
2020-11-18 18:39:38 +0800

    增加MySQL数据库初始化脚本以及说明

--- day-common/src/main/resources/initSeata.sql
+++ day-common/src/main/resources/initSeata.sql
@@ -24,6 +24,9 @@ CREATE TABLE IF NOT EXISTS `undo_log`
 # 为seata服务端的sql语句：
 -- -------------------------------- The script used when storeMode is 'db' --------------------------------
 -- the table to store GlobalSession data
+#首先创建 服务端的数据库
+CREATE DATABASE `seata`;
+#然后创建其服务端的所需要的表
 DROP TABLE IF EXISTS `global_table`;
 CREATE TABLE IF NOT EXISTS `global_table`
 (
@@ -79,3 +82,88 @@ CREATE TABLE IF NOT EXISTS `lock_table`
     KEY `idx_branch_id` (`branch_id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
+
+
+# -----------创建订单表---------------------------------------------------------------
+# 创建  seata_order 数据库
+CREATE DATABASE `seata_order`;
+# 创建订单表
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_order
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order`;
+CREATE TABLE `t_order`
+(
+    `id`             int(11)                                                 NOT NULL AUTO_INCREMENT,
+    `order_no`       varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `user_id`        varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `commodity_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `status`         int(5)                                                  NULL DEFAULT 0,
+    `amount`         double(14, 2)                                           NULL DEFAULT 0.00,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+# --------------创建用户表---------------------------------------------------------
+#创建 seata_pay 数据库
+CREATE DATABASE `seata_pay`;
+#创建用户钱包表
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for account
+-- ----------------------------
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account`
+(
+    `id`      int(11)                                                 NOT NULL AUTO_INCREMENT,
+    `user_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `amount`  double(14, 2)                                           NULL DEFAULT 0.00,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 3
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
+-- ----------------------------
+-- Records of account
+-- ----------------------------
+#初始化数据
+INSERT INTO `account`
+VALUES (1, '1', 4000.00);
+INSERT INTO `account`
+VALUES (2, '1001', 50000.00);
+SET FOREIGN_KEY_CHECKS = 1;
+
+#================================================================================================================================
+#配置nacos，即，从nacos官网中下载稳定版都安装包
+# 1、首先创建数据库
+CREATE DATABASE `nacos_config`;
+# 2、创建表，即执行nacos压缩包中的conf文件夹下的nacos-mysql.sql中的创建表的语句
+# 3、将创建好的数据库，配置到nacos中，如以下配置：
+### Count of DB:
+#   db.num=1
+#
+# ### Connect URL of DB:
+#   db.url.0=jdbc:mysql://127.0.0.1:3306/nacos_config?characterEncoding=utf8&connectTimeout=1000&socketTimeout=3000&autoReconnect=true&useUnicode=true&useSSL=false&serverTimezone=UTC
+#   db.user=root
+#   db.password=123456
+
+# 在本地启动nacos  命令：startup.cmd -m standalone
+# 如果需要在docker中启动nacos，
+# docker中安装nacos1.3.1
+# 主要参考与：
+# 1、 https://www.cnblogs.com/binz/p/12295346.html
+# 2、 https://www.jianshu.com/p/e053f016371a
+# 1. docker search nacos
+# 2. docker pull nacos/nacos-server:1.3.1
+# 3. docker run -d -p 8848:8848 -e MODE=standalone -v /usr/nacos/properties/custom.properties:/home/nacos/init.d/custom.properties -v /usr/nacos/logs:/home/nacos/logs --restart always --name nacos nacos/nacos-server:1.3.1
